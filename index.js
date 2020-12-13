0517fdc0bfad7d6f8ad90b3b7fa2de8670f39151
Jasmine Carter <jasmine.carter87@gmail.com>
2020-12-13 18:50:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  set diameter(radius) {
+    this.diameter = radius + radius
+  }
+  set circumference(radius) {
+    this.circumference = 2 * Math.PI * this.radius
+  }
+}
